f493572eb2bc542c03f4fe6b9876402b20f72560
raiden314 <112748994+raiden314@users.noreply.github.com>
2024-01-13 16:07:19 +0600

    index&Hellopageを実装。送られたパラメータを使用してindexpageを実装しました。

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+
+@Controller
+public class HelloController {
+	@RequestMapping("/{num}")
+	public String index (@PathVariable int num, Model model) {
+		int sum = 0;
+		for (int i = 1; i <= num; i++) {
+			sum += i;
+		}
+		model.addAttribute("msg", "sum=" + sum);
+		return "index";
+	}
+
+    @RequestMapping(value="/hello")
+    public String hello() {
+        return "hello";
+    }
+}
