7f499ffddc07e8246759e571808f8798b7dd6d84
raiden314 <112748994+raiden314@users.noreply.github.com>
2024-01-13 16:35:44 +0600

    Modeから、ModelAndViewに変更して実装。

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,20 +2,21 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
 
 @Controller
 public class HelloController {
 	@RequestMapping("/{num}")
-	public String index (@PathVariable int num, Model model) {
+	public ModelAndView index (@PathVariable int num, ModelAndView mav) {
 		int sum = 0;
 		for (int i = 1; i <= num; i++) {
 			sum += i;
 		}
-		model.addAttribute("msg", "sum=" + sum);
-		return "index";
+		mav.addObject("msg", "sum=" + sum);
+		mav.setViewName("index");
+		return mav;
 	}
 
     @RequestMapping(value="/hello")
