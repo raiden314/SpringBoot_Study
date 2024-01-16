8110c78c73350b04b753b339f5efb1ad6699e864
raiden314 <112748994+raiden314@users.noreply.github.com>
2024-01-16 09:13:37 +0600

    動的ページを作成。

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,24 +1,31 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-	@RequestMapping("/{num}")
-	public ModelAndView index (@PathVariable int num, ModelAndView mav) {
-		int sum = 0;
-		for (int i = 1; i <= num; i++) {
-			sum += i;
-		}
-		mav.addObject("msg", "sum=" + sum);
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView index (ModelAndView mav) {
 		mav.setViewName("index");
+		mav.addObject("msg","お名前を入力してください");
 		return mav;
 	}
-
+	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(
+				@RequestParam("text1")String str,
+				ModelAndView mav) {
+			mav.setViewName("index");
+			mav.addObject("msg", "こんにちは" + str + "さん！");
+			mav.addObject("value",str);
+			return mav;
+			
+	}
+	
     @RequestMapping(value="/hello")
     public String hello() {
         return "hello";
