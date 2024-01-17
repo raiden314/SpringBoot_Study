7fea2b665e9b2652ea20bfbd26dfe473d8bfeb00
raiden314 <112748994+raiden314@users.noreply.github.com>
2024-01-17 17:04:37 +0600

    ルーティングの書き方を変更しました。

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,21 +1,21 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	@GetMapping("/")
 	public ModelAndView index (ModelAndView mav) {
 		mav.setViewName("index");
 		mav.addObject("msg","お名前を入力してください");
 		return mav;
 	}
 	
-	@RequestMapping(value="/", method=RequestMethod.POST)
+	@PostMapping("/")
 	public ModelAndView send(
 				@RequestParam("text1")String str,
 				ModelAndView mav) {
@@ -26,7 +26,7 @@ public class HelloController {
 			
 	}
 	
-    @RequestMapping(value="/hello")
+	@GetMapping("/hello")
     public String hello() {
         return "hello";
     }
